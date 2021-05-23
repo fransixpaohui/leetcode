51c64d49e71ae3c2d2b3575c8c21b006b131c0ae
Kctaig <941388287@qq.com>
2021-05-23 20:52:56 +0800

    a test

--- 43.multiply-strings.cpp
+++ 43.multiply-strings.cpp
@@ -2,13 +2,13 @@
  * @lc app=leetcode id=43 lang=cpp
  *
  * [43] Multiply Strings
- */
+ */s
 
 // @lc code=start
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        
+        wwwwwww
     }
 };
 // @lc code=end
