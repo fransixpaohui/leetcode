2b5129adb1c9f4351fe40e7812f1cceebd50831f
Kctaig <941388287@qq.com>
2021-05-25 11:40:25 +0800

    first commit

--- 79.word-search.cpp
+++ 79.word-search.cpp
@@ -2,26 +2,27 @@
  * @lc app=leetcode id=79 lang=cpp
  *
  * [79] Word Search
+ * 
+ * 写的还是挺费劲的，但是一遍出来了
  */
 
 // @lc code=start
 class Solution {
    public:
-    bool traval(vector<vector<char>>& b, string w, int k,
-                vector<vector<bool>>& memo, int i, int j) {
-        if(k==w.size()-1){
+    bool traval(vector<vector<char>>& b, string w, int k, vector<vector<bool>>& memo, int i, int j) {
+        if(k==w.size()-1){          //递归终点判断
             if(b[i][j]==w[k]){
                 return true;
             }else{
                 return false;
             }
         }
-        if(w[k]!=b[i][j]){
+        if(w[k]!=b[i][j]){          //*不满足递归条件，返回
             return false;
         }
         memo[i][j] = true;
         if (j < b[0].size()-1 && !memo[i][j + 1]) {
-            if (traval(b, w, k + 1, memo, i, j + 1)) {
+            if (traval(b, w, k + 1, memo, i, j + 1)) {     //以下类似结构是为了直接返回true，而不返回false，以继续遍历
                 return true;
             }
         }
